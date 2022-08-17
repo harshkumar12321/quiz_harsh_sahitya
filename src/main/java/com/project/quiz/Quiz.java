3f55715fd0c1c39f8b0205485d0c60d71660523f
harshkumar12321 <109940517+harshkumar12321@users.noreply.github.com>
2022-08-17 11:30:12 +0530

    added project files

---
+++ src/main/java/com/project/quiz/Quiz.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.project.quiz;
+
+/**
+ *
+ * @author 91987
+ */
+public class Quiz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
